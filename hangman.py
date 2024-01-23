4ac231818792f2a041f442a872ef452bc736b553
Ticster <empynia@gmail.com>
2024-01-23 10:20:27 +0100

    moved UI to its own class. fixed issue were victory condition was not met

--- hangman.py
+++ hangman.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 class FileHandler:
     # read and return data from file
@@ -34,19 +33,11 @@ class Ansi:
     
     def reset_screen(self):
         return f"{self.clear_screen}"
-
-class Hangman:
+    
+class HangmanUI:
     def __init__(self):
-        self.files = FileHandler()
+        self.hangman = Hangman()
         self.ansi = Ansi()
-        self.word = None
-        self.char = []
-        self.guessed_char = []
-        self.incorrect_guessed_chars = []
-        self.incorrect_guesses = 0
-        self.correct_guess = 0
-        self.game_state = True
-        self._random_word()
         self.stages = [
             """
             -----
@@ -112,7 +103,50 @@ class Hangman:
            ---
             """,
         ]
+        
 
+    def draw_hangman(self):
+        current_stage = min(self.hangman.incorrect_guesses, len(self.stages) - 1)
+        print(self.stages[current_stage])
+
+    # draws _ where character not guessed. puts correct guessed characters instead of _
+    def draw_line(self):
+        game_result = self.hangman.game_lost()
+        for i in self.hangman.word:            
+            if i in self.hangman.guessed_char:
+                print(self.ansi.text_color(color=self.ansi.green, text=f"{i} "), end="")
+            elif game_result:
+                print(self.ansi.text_color(color=self.ansi.red, text=i,), end=" ")
+            else:
+                print(self.ansi.text_color(color=self.ansi.bright_cyan, text="_ "), end="")
+            
+        print()
+
+    # Wrapper functions for class Hangman
+    def split_user_guess(self, guess):
+        self.hangman.split_user_guess(guess)
+
+    def game_end(self):
+        return self.hangman.game_end()
+    
+    def user_guess(self, guess):
+        self.hangman.user_guess(guess)
+    # Wrapper functions end
+
+
+class Hangman:
+    def __init__(self):
+        self.files = FileHandler()
+        self.ansi = Ansi()
+        self.word = None
+        self.char = set()
+        self.guessed_char = []
+        self.incorrect_guessed_chars = []
+        self.incorrect_guesses = 0
+        self.correct_guess = 0
+        self.game_state = True
+        self._random_word()
+        
     # variable update
     def _update_guessed_chars(self, guessed_char):
         if not guessed_char in self.guessed_char:
@@ -130,7 +164,7 @@ class Hangman:
     
     # splits word into characters
     def _letters_in_word(self):
-        self.char = [char for char in self.word.strip()]
+        self.char = {char for char in self.word.strip()}
 
     # picks random word from words.txt
     def _random_word(self):
@@ -141,9 +175,9 @@ class Hangman:
 
     # reset game loop. else ends program
     def _restart(self):
-        usr = input(self.ansi.text_color(color=self.ansi.yellow, text="Restart? y/n: ")).lower()
-        #usr = input("Restart? ").lower()
-        if usr == 'y':
+        user = input(self.ansi.text_color(color=self.ansi.yellow, text="Restart? y/n: ")).lower()
+        if user == 'y':
+            print(self.ansi.reset_screen())
             self.correct_guess = 0
             self.incorrect_guesses = 0
             self.incorrect_guessed_chars = []
@@ -151,45 +185,33 @@ class Hangman:
             self._random_word()
             self.game_state = True
         else:
+            print(self.ansi.reset_screen())
             self.game_state = False
-
-    def draw_hangman(self):
-        current_stage = min(self.incorrect_guesses, len(self.stages) - 1)
-        print(self.stages[current_stage])
-
-    # draws _ where character not guessed. puts correct guessed characters instead of _
-    def draw_line(self):
-        for i in self.char:
-            if i in self.guessed_char:
-                print(self.ansi.text_color(color=self.ansi.green, text=f"{i} "), end="")
-            else:
-                print(self.ansi.text_color(color=self.ansi.bright_cyan, text="_ "), end="")
-        print()
     
     # checks if user guess is correct or not
-    def usr_guess(self, guess):
+    def user_guess(self, guess):
         if guess in self.char:
-            print(self.ansi.text_color(color=self.ansi.green, text=f"{guess} is correct"))
             self._update_guessed_chars(guess)
-            # time.sleep(1)
-            # print(self.ansi.clear_screen)
         elif not guess in self.char:
-            print(self.ansi.text_color(color=self.ansi.red, text=f"{guess} is not correct"))
             self._update_incorrect_chars(guess)
-            # time.sleep(1)
-            # print(self.ansi.clear_screen)
     
     # splits user string input into char.
     def split_user_guess(self, guess):
         for char in guess:
-            self.usr_guess(char)
+            self.user_guess(char)
 
-    # game over / victory evaluation (defeat based on img in stages list)
+    def game_lost(self):
+        return self.incorrect_guesses >= 7
+    
+    def game_won(self):
+        return self.correct_guess == len(self.char)
+
+    # game over / victory evaluation (defeat based on amount of img in stages list)
     def game_end(self):
-        if self.incorrect_guesses >= 7:
+        if self.game_lost():
             print(self.ansi.text_color(color=self.ansi.red, text="Game Over!"))
             self._restart()
-        elif self.correct_guess == len(self.char):
+        elif self.game_won():
             print(self.ansi.text_color(color=self.ansi.green, text="Victory!"))
             self._restart()
         else:
@@ -197,18 +219,20 @@ class Hangman:
 
 # main program loop
 if __name__ == "__main__":
-    hangman = Hangman()
+    hangmanui = HangmanUI()
 
-    while hangman.game_state:
-        hangman.draw_hangman()
-        hangman.draw_line()
+    while hangmanui.hangman.game_state:
+        hangmanui.draw_hangman()
+        hangmanui.draw_line()
+        if hangmanui.game_end():
+            user_guess = input(hangmanui.ansi.text_color(color=hangmanui.ansi.bright_cyan, text="Enter Guess: ")).lower()
+            
+            if len(user_guess) > 1:
+                hangmanui.split_user_guess(user_guess)
+            else:
+                hangmanui.user_guess(user_guess)
+
+            print(hangmanui.ansi.reset_screen())
         
-        user_guess = input(hangman.ansi.text_color(color=hangman.ansi.bright_cyan, text="Enter Guess: "))
-        if len(user_guess) > 1:
-            hangman.split_user_guess(user_guess)
-            hangman.game_end()
-        else:
-            hangman.usr_guess(user_guess)
-            hangman.game_end()
     
-
+    
